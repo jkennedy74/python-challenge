8bbdca328dcf9879d70c1ddcb1c3a059bf959321
jkennedy74 <33709673+jkennedy74@users.noreply.github.com>
2018-05-05 07:02:37 -0500

    commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,55 +1,21 @@
-import os
-import csv
-
-# Set path -- why do we need this??
-csvpath = os.path.join("../Resources/election_data_1.csv")
-csvpath2 = os.path.join("../Resources/election_data_2.csv")
-
-# Lists to store data
-voter_id = []
-county = []
-candidate = []
-total_votes = 1
-
-# Read file 1
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-
-    for row in csvreader:
-        total_votes = total_votes +1
-        voter_id.append(row[0])
-        county.append(row[1])
-        candidate.append(row[2])
+import pandas as pd
+df = pd.read_csv("../Resources/election_data_1.csv")
+df2 = pd.read_csv("../Resources/election_data_2.csv")
 
 
-# Read file 2
-with open(csvpath2, newline="") as csvfile:
-    csvreader2 = csv.reader(csvfile, delimiter=",")
+poll_data = df.append(df2)
 
-    for row in csvreader2:
-        total_votes = total_votes + 1
-        voter_id.append(row[0])
-        county.append(row[1])
-        candidate.append(row[2])
+# Get total votes
+total_votes = sum(poll_data["Voter ID"].value_counts())
+print(total_votes)
 
+# Get complete list of candidates that received votes
+all_votes = poll_data['Candidate'].value_counts()
+print(all_votes)
 
-
-# Zip lists together
-cleaned_csv = zip(voter_id, county, candidate)
+pr = all_votes/total_votes*100
+print(pr)
 
 
-# Do some math
-# Total votes from counter above in reader loops
-print(total_votes) 
-
-
-#Set output file path
-output_file = os.path.join("../output/election_data.csv")
-
-#Write to file
-with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
-    writer.writerows(cleaned_csv)
-
-print("Yep, I'm slow.  But I'm done")
-
+winner = all_votes.nlargest(1)
+print(winner)
